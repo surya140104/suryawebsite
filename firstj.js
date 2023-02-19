e0789a478216f20812b47aad5de84027e0617dfe
surya140104 <111858373+surya140104@users.noreply.github.com>
2023-02-19 15:21:08 +0530

    first commit

---
+++ firstj.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
